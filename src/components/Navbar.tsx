56c4204f305808f260706b4fff2ac316bc25079a
Alishba-Siddique <alishbasiddique07@gmail.com>
2024-04-26 01:01:23 +0500

    make the app more responsive

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,32 @@
 import React, { useState } from 'react';
-import HistorySection from './HistorySection';
 import styles from '@/styles/Navbar.module.css';
+import HistorySection from './HistorySection';
 
-const Navbar = () => {
+interface NavbarProps {
+  onClose: () => void;
+}
+
+const Navbar: React.FC<NavbarProps> = ({ onClose }) => {
   const [showHistory, setShowHistory] = useState(false);
+  const [navbarVisible, setNavbarVisible] = useState(false);
+
+  const toggleNavbar = () => {
+    setNavbarVisible(!navbarVisible);
+  };
 
   const toggleHistory = () => {
     setShowHistory(!showHistory);
+    toggleNavbar();
   };
 
   return (
-    <div className={styles.navbar}>
+    <div className={navbarVisible ? styles.navbar : styles.navbarHidden}>
       <div className={styles.hamburgerMenu} onClick={toggleHistory}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="w-6 h-6">
           <path strokeLinecap="round" strokeLinejoin="round" d="M4 6h16M4 12h16m-7 6h7" />
         </svg>
       </div>
-      {showHistory && <HistorySection />}
+      {showHistory && <HistorySection onClose={onClose} />}
     </div>
   );
 };
